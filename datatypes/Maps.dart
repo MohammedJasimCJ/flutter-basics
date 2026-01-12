cd48d3c96dd7412cacf9006b6c15d02782150b0f
Mohammed Jasim <jasimjasu2002@gmail.com>
2026-01-12 21:05:23 +0530

    Add  example of Maps

---
+++ datatypes/Maps.dart
@@ -0,0 +1,8 @@
+void main(){
+  Map<String,String> myDetails={
+    'name':'Rahul',
+       'address':'India',
+         'fathername':'Soman'
+            };
+            print(myDetails['name']);
+         }
